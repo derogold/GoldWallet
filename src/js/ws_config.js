9f6dfd25bd0beca5d081e79511817f8ca0051962
LeoStehlik <leo@sheerness-enterprises.co.uk>
2019-11-13 18:36:01 +0100

    Initial commit

--- src/js/ws_config.js
+++ src/js/ws_config.js
@@ -1,23 +1,23 @@
 var config = {};
 
 // self explanatory, your application name, descriptions, etc
-config.appName = 'WalletShell';
-config.appDescription = 'TurtleCoin Wallet';
-config.appSlogan = 'Slow and steady wins the race!';
-config.appId = 'lol.turtlecoin.walletshell';
-config.appGitRepo = 'https://github.com/turtlecoin/turtle-wallet-electron';
+config.appName = 'GoldWallet';
+config.appDescription = 'DeroGold Wallet';
+config.appSlogan = 'DeroGold. For a Better Tomorrow!';
+config.appId = 'derogold.goldwallet';
+config.appGitRepo = 'https://github.com/derogold/GoldWallet.git';
 
 // default port number for your daemon (e.g. TurtleCoind)
-config.daemonDefaultRpcPort = 11898;
+config.daemonDefaultRpcPort = 6969;
 
 // wallet file created by this app will have this extension
-config.walletFileDefaultExt = 'twl';
+config.walletFileDefaultExt = 'wallet';
 
 // change this to match your wallet service executable filename
-config.walletServiceBinaryFilename = 'turtle-service';
+config.walletServiceBinaryFilename = 'DeroGold-service';
 
 // version on the bundled service (turtle-service)
-config.walletServiceBinaryVersion = "v0.13.0";
+config.walletServiceBinaryVersion = "v0.4.0";
 
 // config file format supported by wallet service, possible values:
 // ini -->  for turtle service (or its forks) version <= v0.8.3
@@ -25,54 +25,53 @@ config.walletServiceBinaryVersion = "v0.13.0";
 config.walletServiceConfigFormat = "json";
 
 // default port number for your wallet service (e.g. turtle-service)
-config.walletServiceRpcPort = 8070;
+config.walletServiceRpcPort = 1337;
 
 // block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
-config.blockExplorerUrl = 'https://explorer.turtlecoin.lol/transaction.html?hash=[[TX_HASH]]';
+config.blockExplorerUrl = 'http://derogold4ever.online/transaction.html?hash=[[TX_HASH]]';
 
 // default remote node to connect to, set this to a known reliable node for 'just works' user experience
-config.remoteNodeDefaultHost = 'turtlenode.co';
+config.remoteNodeDefaultHost = 'derogold4ever.online';
 
 
 // remote node list update url, set to null if you don't have one
-// for TRTL:
-// raw list: https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json
-// filtered: https://trtl.nodes.pub/api/getNodes
-config.remoteNodeListUpdateUrl = 'https://trtl.nodes.pub/api/getNodes';
+// for DEGO:
+// raw list: https://raw.githubusercontent.com/derogold/derogold-nodes-json/master/derogold-nodes.json
+config.remoteNodeListUpdateUrl = 'https://raw.githubusercontent.com/derogold/derogold-nodes-json/master/derogold-nodes.json';
 
 // set to false if using raw/unfiltered node list
-config.remoteNodeListFiltered = true;
+config.remoteNodeListFiltered = false;
 
 // fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
 config.remoteNodeListFallback = [
-  'turtlenode.co:11898',
-  'nodes.hashvault.pro:11898',
-  'turtle.mine.nu:11898',
+  'publicnode.ydns.eu:6969',
+  'derogold4ever.online:6969',
+  'node.stx.nl:6969',
 ];
 config.remoteNodeCacheSupported = false;
 config.remoteNodeSslSupported = false;
 
 // your currency name
-config.assetName = 'TurtleCoin';
+config.assetName = 'DeroGold';
 // your currency ticker
-config.assetTicker = 'TRTL';
+config.assetTicker = 'DEGO';
 // your currency address prefix, for address validation
-config.addressPrefix = 'TRTL';
+config.addressPrefix = 'dg';
 // standard wallet address length, for address validation
 config.addressLength = 99;
 // integrated wallet address length, for address validation. Added length is length of payment ID encoded in base58.
-config.integratedAddressLength = config.addressLength + ((64 * 11) / 8);
+config.integratedAddressLength = 187;
 
 // minimum fee for sending transaction
-config.minimumFee = 0.1;
+config.minimumFee = 10000;
 // minimum amount for sending transaction
-config.mininumSend = 0.1;
+config.mininumSend = 1000;
 // default mixin/anonimity for transaction
 config.defaultMixin = 3;
 // to represent human readable value
 config.decimalPlaces = 2;
 // to convert from atomic unit
-config.decimalDivisor = 10 ** config.decimalPlaces;
+config.decimalDivisor = 100;
 
 // obfuscate address book entries, set to false if you want to save it in plain json file.
 // not for security because the encryption key is attached here
@@ -82,8 +81,8 @@ config.addressBookObfuscationKey = '79009fb00ca1b7130832a42de45142cf6c4b7f333fe6
 // initial/sample entries to fill new address book
 config.addressBookSampleEntries = [
   {
-    name: 'WalletShell Donation',
-    address: 'TRTLv1A26ngXApin33p1JsSE9Yf6REj97Xruz15D4JtSg1wuqYTmsPj5Geu2kHtBzD8TCsfd5dbdYRsrhNXMGyvtJ61AoYqLXVS',
+    name: 'GoldWallet Donation',
+    address: 'dg47xN74St6btXZUDo96NW7G62djERbst3eFuiQCaMo1AADep62Siqu3vmnDcc3tFXf1wgnVKvGwD1eyYZqYBhrB2ChcBtGJL',
     paymentId: '',
   }
 ];
